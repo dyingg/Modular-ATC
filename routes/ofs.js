1752371a7d4481edcce7197562405c7a2b52a8c8
dying <inktechser@gmail.com>
2019-06-16 04:21:36 +0530

    pad fix

--- routes/ofs.js
+++ routes/ofs.js
@@ -4,10 +4,13 @@ const router = express.Router();
 router.get("/:pid/:size", (req, res) => {
   let size = req.params.size;
   let pid = req.params.pid;
-  size = size.split(".").join("");
-  size = size.padEnd(2, "0");
-  size = size.padStart(3, "0");
+  if (!size || !pid) return res.end("Invalid call - exited!");
 
+  let sizeBuilder = size.split(".");
+  size = sizeBuilder[0].padStart(2, "0");
+  if (sizeBuilder[1]) {
+    size += sizeBuilder[1];
+  }
   res.redirect(
     "https://www.offspring.co.uk/view/product/offspring_catalog/5,22/" +
       pid +
